268fa73031b675942db74e467965f6d72ff401b8
Alejandro MG <alemangui@gmail.com>
2019-12-02 16:27:52 +0100

    Adds vuex-persistedstate to persist the VueJS web app's state between reloads.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import Constants from '@/constants'
 import formutils from '@/formutils'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     formDefinitionsLoadingStatus: Constants.LoadingStatus.LOADING,
     suggestionsLoadingStatus: Constants.LoadingStatus.IDLE,
