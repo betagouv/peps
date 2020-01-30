258f607164a63a8b4cfc717e497c3359010d9d5a
Alejandro MG <alemangui@gmail.com>
2020-01-30 14:39:14 +0100

    Fixes vue router navigation issue with IE11 (#95)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueBrowserUpdate, {
   options: {
-    required: {i: 11},
+    required: { i: 11 },
     noclose: true,
   }
 })
@@ -20,9 +20,24 @@ Vue.http.interceptors.push((request) => {
   request.headers['X-CSRFToken'] = window.CSRF_TOKEN
 })
 
+// router and IE 11 workaround. see: https://github.com/vuejs/vue-router/issues/1911
+const IE11RouterFix = {
+  methods: {
+    hashChangeHandler: function () {
+      this.$router.push(window.location.hash.substring(1, window.location.hash.length))
+    },
+    isIE11: function () {
+      return !!window.MSInputMethodContext && !!document.documentMode
+    }
+  },
+  mounted: function () { if (this.isIE11()) { window.addEventListener('hashchange', this.hashChangeHandler); } },
+  destroyed: function () { if (this.isIE11()) { window.removeEventListener('hashchange', this.hashChangeHandler); } }
+};
+
 new Vue({
   vuetify,
   store,
   router,
+  mixins: [IE11RouterFix],
   render: h => h(App)
 }).$mount('#app')
