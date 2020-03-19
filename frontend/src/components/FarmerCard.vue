49267feaa0a81fbd7f66102a2e9b46a8084a828f
Alejandro MG <alemangui@gmail.com>
2020-03-19 13:57:25 +0100

    Fixes gradient issue from farmer card. Adds links to experiments.

--- frontend/src/components/FarmerCard.vue
+++ frontend/src/components/FarmerCard.vue
@@ -5,7 +5,7 @@
       :src="farmer.backgroundPhoto"
       height="100px"
       style="background: #CCC;"
-    ></v-img> -->
+    ></v-img>-->
     <v-list-item class="flex-fix-item" style="margin: 10px 0 0 0;">
       <v-list-item-avatar color="grey">
         <v-img :src="farmer.profile_image" v-if="farmer.profile_image"></v-img>
@@ -22,21 +22,28 @@
         </v-list-item-title>
         <v-list-item-subtitle>
           <span v-for="(title, index) in (farmer.profession || [])" :key="index">
-          {{ title }}<span v-if="farmer.profession.length > 1 && index < farmer.profession.length - 1">, </span>
+            {{ title }}
+            <span v-if="farmer.profession.length > 1 && index < farmer.profession.length - 1">,</span>
           </span>
         </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
-    <v-card-subtitle class="subtitle-2 flex-fix-item" style="padding: 4px 16px 4px 16px;">Son exploitation</v-card-subtitle>
+    <v-card-subtitle
+      class="subtitle-2 flex-fix-item"
+      style="padding: 4px 16px 4px 16px;"
+    >Son exploitation</v-card-subtitle>
     <v-card-text class="description flex-shrink-item">
       {{ farmer.description }}
       <div class="gradient" v-if="showDescriptionGradient"></div>
     </v-card-text>
-    <v-card-subtitle class="subtitle-2 flex-fix-item" style="padding: 4px 16px 4px 16px;">Expérimentations</v-card-subtitle>
+    <v-card-subtitle
+      class="subtitle-2 flex-fix-item"
+      style="padding: 4px 16px 4px 16px;"
+    >Expérimentations</v-card-subtitle>
     <v-card-text class="description flex-fix-item">
       <ul>
         <li v-for="(item, index) in farmer.experiments" :key="index">
-          {{ item.name }}
+          <a :href="'/agriculteur/' + farmer.name + '/experimentation/' + item.name">{{ item.name }}</a>
         </li>
       </ul>
     </v-card-text>
@@ -72,13 +79,20 @@ export default {
         name: "Farmer",
         params: { farmerName: farmer.name }
       })
+    },
+    adjustHeight() {
+      if (this.$el) {
+        const domElement = this.$el.querySelector(".description")
+        this.showDescriptionGradient =
+          domElement.scrollHeight > domElement.offsetHeight
+      }
     }
   },
+  updated() {
+    this.adjustHeight()
+  },
   mounted() {
-    if (this.$el) {
-      const domElement = this.$el.querySelector(".description")
-      this.showDescriptionGradient = domElement.scrollHeight > domElement.offsetHeight
-    }
+    this.adjustHeight()
   }
 }
 </script>
