06725959ea02572eea3a8e95ab8e2dea6409e576
Alejandro MG <alejandro@amguillen.dev>
2020-08-05 15:31:18 +0200

    Reverts Gzip on back and front

--- peps/settings.py
+++ peps/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
