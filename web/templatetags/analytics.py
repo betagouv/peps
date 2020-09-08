9e47e5835f7b648639ae673b959ae887922186ce
Alejandro MG <alejandro@amguillen.dev>
2020-09-08 16:14:24 +0200

    Revert "Adds a different environment variable to match exactly the domain for matomo"
    This reverts commit 2ec2689621aa5131f3f700caadad620b37c03f38.

--- web/templatetags/analytics.py
+++ web/templatetags/analytics.py
@@ -8,9 +8,5 @@ def analytics_id():
     return getattr(settings, 'ANALYTICS_ID', '')
 
 @register.simple_tag
-def analytics_cookie_domain():
-    return getattr(settings, 'ANALYTICS_COOKIE_DOMAIN')
-
-@register.simple_tag
 def analytics_domain():
     return getattr(settings, 'ANALYTICS_DOMAIN')
