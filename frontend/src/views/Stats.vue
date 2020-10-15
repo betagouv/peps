2eba40f2cc0d8ad48b4637ef27caa4da735c0a82
Alejandro MG <alejandro@amguillen.dev>
2020-10-15 10:21:26 +0200

    Fixes http scheme for metabase stats

--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -3,7 +3,7 @@
     <Title :title="title" :breadcrumbs="breadcrumbs" />
     <v-container class="constrained">
       <iframe
-          src="http://peps-metabase.cleverapps.io/public/dashboard/00536261-3e78-4d92-aa9b-a5996c5bf8fb"
+          src="https://peps-metabase.cleverapps.io/public/dashboard/00536261-3e78-4d92-aa9b-a5996c5bf8fb"
           style="border: 1px solid rgb(204, 204, 204);box-shadow: 3px 3px 7px -3px #999;"
           width="100%"
           height="1100"
